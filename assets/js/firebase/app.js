45112ec71415a65ec3d7ec45d6526e322df0831b
unknown <mateus.r2000@gmail.com>
2022-09-06 15:23:18 -0300

    first commit

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBruTRWzdY8_IQJG4KA3gVoGK1SpPmfGos",
+    authDomain: "inverted-world-81ba5.firebaseapp.com",
+    projectId: "inverted-world-81ba5",
+    storageBucket: "inverted-world-81ba5.appspot.com",
+    messagingSenderId: "760373765574",
+    appId: "1:760373765574:web:83c01696b984b07101bec7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default app;
